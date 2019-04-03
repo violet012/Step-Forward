35df279270f8b45f21104386c2cb92a7770373db
Cole Thiessen <ckthiessen@hotmail.com>
2019-04-03 15:03:20 -0600

    Prototype version 1.0

---
+++ script.js
@@ -0,0 +1,25 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("showcase").style.marginLeft = "250px";
+    document.getElementById("about").style.marginLeft = "250px";
+    document.getElementById("ourVolunteers").style.marginLeft = "250px";
+    document.getElementById("Sponsorships").style.marginLeft = "250px";
+    document.getElementById("theTeam").style.marginLeft = "250px";
+    document.getElementById("calendar").style.marginLeft = "250px";
+    document.getElementById("signUp").style.marginLeft = "250px";
+    document.getElementById("feedback").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("showcase").style.marginLeft = "0";
+    document.getElementById("about").style.marginLeft = "0";
+    document.getElementById("ourVolunteers").style.marginLeft = "0";
+    document.getElementById("Sponsorships").style.marginLeft = "0";
+    document.getElementById("theTeam").style.marginLeft = "0";
+    document.getElementById("calendar").style.marginLeft = "0";
+    document.getElementById("signUp").style.marginLeft = "0";
+    document.getElementById("feedback").style.marginLeft = "0";
+}
